36405b5feedd54c6cc90a75c23e2c1e485dabae0
akholod <andrey.kokhanevich@gmail.com>
2017-03-07 16:51:27 +0200

    fix bugs

--- src/components/ChooseSeasson.js
+++ src/components/ChooseSeasson.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class ChooseSeasson extends Component {
     buttonClickHandler = (arg) => {
         this.props.changeLeagueHandler(arg);
     }
